ab8865f03206fee1756a089cdf1be5384afb06f7
OliveAlila <oliveayumaalila@gmail.com>
2024-04-29 12:14:09 +0300

    Initial commit

---
+++ app/src/main/java/com/olive/firebase/navigation/Routes.kt
@@ -0,0 +1,9 @@
+package com.olive.firebase.navigation
+
+const val ROUTE_LOGIN="login"
+const val ROUTE_REGISTER="register"
+const val ROUTE_HOME="home"
+const val ROUTE_VIEWPRODUCTS="ViewProducts"
+const val ROUTE_UPDATEPRODUCTS="UpdateProducts"
+const val ROUTE_ADDPRODUCTS="AddProducts"
+const val ROUTE_UPLOADPRODUCTS="ViewUploadProducts"
